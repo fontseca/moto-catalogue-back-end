4e71176b8064e09109ea7c537ac6ea63f5bd914d
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-05-10 15:55:39 -0600

    Set up database.
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+  "database/sql"
+  _ "github.com/mattn/go-sqlite3"
+  "log"
+)
+
 func main() {
+  db, err := sql.Open("sqlite3", "db.sqlite")
+  if nil != err {
+    log.Fatalf("could not open database: %v", err)
+  }
+
+  defer db.Close()
+
+  err = db.Ping()
+  if nil != err {
+    log.Fatalf("could not ping database: %v", err)
+  }
 
 }
