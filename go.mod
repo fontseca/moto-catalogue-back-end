ac00afd2c39fec1ea6e8e6b208662e4dfd761a42
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-05-10 21:47:27 -0600

    Use middlewares.
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/mattn/go-sqlite3 v1.14.22
 	golang.org/x/crypto v0.23.0
 )
+
+require github.com/golang-jwt/jwt/v5 v5.2.1
--- main.go
+++ main.go
@@ -1,15 +1,80 @@
 package main
 
 import (
+  "context"
   "database/sql"
+  "github.com/golang-jwt/jwt/v5"
   _ "github.com/mattn/go-sqlite3"
   "log"
+  "log/slog"
   "net"
   "net/http"
+  "os"
+  "strings"
   "time"
 )
 
+func setHeader(key, value string) func(http.Handler) http.Handler {
+  return func(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+      w.Header().Add(key, value)
+      next.ServeHTTP(w, r)
+    })
+  }
+}
+
+func withAuthorization(next http.Handler) http.HandlerFunc {
+  secret := os.Getenv("JWT_SECRET")
+  if "" == secret {
+    secret = "default secret"
+  }
+
+  return func(w http.ResponseWriter, r *http.Request) {
+    authorization := r.Header.Get("Authorization")
+    if "" == authorization {
+      w.Header().Set("WWW-Authenticate", "Bearer realm=\"access to system\"")
+      w.WriteHeader(http.StatusUnauthorized)
+      return
+    }
+
+    tokenStr := strings.Split(authorization, " ")[1]
+    token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (any, error) { return []byte(secret), nil })
+    if nil != err {
+      slog.Error(err.Error())
+      w.WriteHeader(http.StatusInternalServerError)
+      return
+    }
+
+    if !token.Valid {
+      w.WriteHeader(http.StatusInternalServerError)
+      return
+    }
+
+    claims := token.Claims.(jwt.MapClaims)
+    userID := claims["user_id"].(int)
+
+    ctx := context.WithValue(r.Context(), "user_id", userID)
+    r = r.Clone(ctx)
+
+    next.ServeHTTP(w, r)
+  }
+}
+
+func with(mux *http.ServeMux, middlewares ...func(http.Handler) http.Handler) http.Handler {
+  var h http.Handler = mux
+
+  for _, middleware := range middlewares {
+    if nil != middleware {
+      h = middleware(h)
+    }
+  }
+
+  return h
+}
+
 func main() {
+  log.SetFlags(log.LstdFlags | log.Lshortfile)
+
   db, err := sql.Open("sqlite3", "db.sqlite")
   if nil != err {
     log.Fatalf("could not open database: %v", err)
@@ -31,9 +96,11 @@ func main() {
 
   defer listener.Close()
 
+  h := with(mux, setHeader("Content-Type", "application/json"))
+
   server := http.Server{
     Addr:              "",
-    Handler:           mux,
+    Handler:           h,
     ReadTimeout:       5 * time.Minute,
     ReadHeaderTimeout: 5 * time.Minute,
     WriteTimeout:      5 * time.Minute,
