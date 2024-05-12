dc61e4148b7f061f8e7fc018d60525f5954bf961
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-05-11 21:59:15 -0600

    Retrieve motorcycles of authenticated user.
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- motorcycle.go
+++ motorcycle.go
@@ -26,15 +26,15 @@ type Motorcycle struct {
   Description string             `json:"description"`
   Location    string             `json:"location"`
   Images      []*MotorcycleImage `json:"images"`
-  CreatedAt   time.Time          `json:"created_at"`
-  UpdatedAt   time.Time          `json:"updated_at"`
+  CreatedAt   string             `json:"created_at"`
+  UpdatedAt   string             `json:"updated_at"`
 }
 
 type MotorcycleImage struct {
-  ID        int       `json:"id"`
-  URL       string    `json:"url"`
-  CreatedAt time.Time `json:"created_at"`
-  UpdatedAt time.Time `json:"updated_at"`
+  ID        int    `json:"id"`
+  URL       string `json:"url"`
+  CreatedAt string `json:"created_at"`
+  UpdatedAt string `json:"updated_at"`
 }
 
 type MotorcycleCreation struct {
@@ -126,6 +126,126 @@ func (s *MotorcycleService) Create(ctx context.Context, ownerID int, creation *M
   return insertedID, nil
 }
 
+func (s *MotorcycleService) GetFromUser(ctx context.Context, ownerID, page int) (motorcycles []*Motorcycle, err error) {
+  getMotorcyclesImagesQuery := `
+       SELECT mi.motorcycle_id, 
+              mi.id,
+              mi.url,
+              mi.created_at,
+              mi.updated_at
+       FROM motorcycle_image mi
+  LEFT JOIN motorcycle m
+         ON m.id = mi.motorcycle_id  
+      WHERE m.owner_id = $1;`
+
+  ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+  defer cancel()
+
+  result, err := s.db.QueryContext(ctx, getMotorcyclesImagesQuery, ownerID)
+  if nil != err {
+    slog.Error(err.Error())
+    return nil, err
+  }
+
+  defer result.Close()
+
+  motorcycleImagesDictionary := map[int][]*MotorcycleImage{}
+
+  for result.Next() {
+    var (
+      motorcycleID int
+      image        MotorcycleImage
+    )
+
+    err = result.Scan(
+      &motorcycleID,
+      &image.ID,
+      &image.URL,
+      &image.CreatedAt,
+      &image.UpdatedAt,
+    )
+
+    if nil != err {
+      slog.Error(err.Error())
+      return nil, err
+    }
+
+    motorcycleImagesDictionary[motorcycleID] = append(motorcycleImagesDictionary[motorcycleID], &image)
+  }
+
+  getUserMotorcyclesQuery := `
+  SELECT id,
+         owner_id,
+         post_title,
+         price,
+         type,
+         mileage,
+         brand,
+         model,
+         year,
+         engine,
+         color,
+         description,
+         location,
+         created_at,
+         updated_at
+    FROM motorcycle
+   WHERE owner_id = @owner_id
+   ORDER BY created_at DESC
+   LIMIT 10
+   OFFSET 10 * (@page - 1);`
+
+  ctx, cancel = context.WithTimeout(ctx, 5*time.Second)
+  defer cancel()
+
+  if 0 >= page {
+    page = 1
+  }
+
+  result, err = s.db.QueryContext(ctx, getUserMotorcyclesQuery,
+    sql.Named("owner_id", ownerID), sql.Named("page", page))
+  if nil != err {
+    slog.Error(err.Error())
+    return nil, err
+  }
+
+  defer result.Close()
+
+  motorcycles = make([]*Motorcycle, 0)
+
+  for result.Next() {
+    var motorcycle Motorcycle
+
+    err = result.Scan(
+      &motorcycle.ID,
+      &motorcycle.OwnerID,
+      &motorcycle.PostTitle,
+      &motorcycle.Price,
+      &motorcycle.Type,
+      &motorcycle.Mileage,
+      &motorcycle.Brand,
+      &motorcycle.Model,
+      &motorcycle.Year,
+      &motorcycle.Engine,
+      &motorcycle.Color,
+      &motorcycle.Description,
+      &motorcycle.Location,
+      &motorcycle.CreatedAt,
+      &motorcycle.UpdatedAt,
+    )
+
+    if nil != err {
+      slog.Error(err.Error())
+      return nil, err
+    }
+
+    motorcycle.Images = motorcycleImagesDictionary[motorcycle.ID]
+    motorcycles = append(motorcycles, &motorcycle)
+  }
+
+  return motorcycles, nil
+}
+
 type MotorcycleHandler struct {
   s *MotorcycleService
 }
@@ -154,4 +274,31 @@ func (h *MotorcycleHandler) Create(w http.ResponseWriter, r *http.Request) {
 
   w.WriteHeader(http.StatusCreated)
   w.Write([]byte(`{ "inserted_id":` + strconv.Itoa(insertedID) + `}`))
+}
+
+func (h *MotorcycleHandler) Get(w http.ResponseWriter, r *http.Request) {
+  ownerID := r.Context().Value("user_id").(int)
+  pageStr := r.URL.Query().Get("page")
+
+  page, err := strconv.Atoi(pageStr)
+  if nil != err {
+    slog.Error(err.Error())
+    page = 1
+  }
+
+  motorcycles, err := h.s.GetFromUser(r.Context(), ownerID, page)
+  if nil != err {
+    w.WriteHeader(http.StatusInternalServerError)
+    return
+  }
+
+  response, err := json.Marshal(motorcycles)
+  if nil != err {
+    slog.Error(err.Error())
+    w.WriteHeader(http.StatusInternalServerError)
+    return
+  }
+
+  w.WriteHeader(http.StatusOK)
+  w.Write(response)
 }
